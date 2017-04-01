9908263c371269172e7e68a903f1c03677d61ab3
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2017-04-01 15:10:56 -0400

    T think this is it

--- server.py
+++ server.py
@@ -1,18 +1,19 @@
 from flask import Flask, request
 import json
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/likes')  # , methods=['POST']
+@app.route('/')  # , methods=['POST']
 def func():
     # likeData = request.data
     # create_json = json.dumps(likeData.json(), sort_keys=True, indent=4, separators=(',', ': '))
     # json_data = json.loads(create_json)
     # print(json_data)
-
-    print("Hello")
+    return "Hello"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
