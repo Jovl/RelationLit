faae24f1ca12af7ceea3cbbf4dac9745fcada92d
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2017-04-01 14:14:07 -0400

    Setting up deployment branch for project

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/likes', methods=['POST'])
+def func():
+    likeData = request.data
+    create_json = json.dumps(likeData.json(), sort_keys=True, indent=4, separators=(',', ': '))
+    json_data = json.loads(create_json)
+    print(json_data)
+
+
+if __name__ == "__main__":
+    app.run()               # replace with host='0.0.0.0', port=80
