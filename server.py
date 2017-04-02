d49e96bbea58fa5473b8840f26fd7e7b19365899
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2017-04-01 23:14:23 -0400

    Testing

--- server.py
+++ server.py
@@ -2,25 +2,32 @@ from flask import Flask, request
 import json
 import os
 
+users = {}
 app = Flask(__name__)
+# connect('RelationLitDB')
+
+userNames = ['User 1', 'User 2', 'User 3', 'User 4']
 
 
 @app.route('/', methods=['POST'])
 def func():
     nameList = []
     genreList = []
+    likeList = []
+
     if request.method == 'POST':
         likeData = request.get_json()
         create_json = json.dumps(likeData, sort_keys=True, indent=4, separators=(',', ': '))
         json_data = json.loads(create_json)
-        # print(json_data)
 
         for i in range(5):
             nameList.append(json_data["items"][i]["name"])
             genreList.append(json_data["items"][i]["genres"])
+            likeList.append(json_data["items"][i]["likes"])
 
-        print nameList
-        print genreList
+        users[userNames.pop()] = [nameList, genreList, likeList]
+
+        print(users)
 
     return "All Good"
 
