e29040a52f71973ae03e636c6353c5b7f48dcbf6
Jeremiah Lantzer <jeremiahlantzer25@gmail.com>
2017-04-01 20:35:03 -0400

    Testing

--- server.py
+++ server.py
@@ -13,13 +13,13 @@ def func():
     if request.method == 'POST':
         likeData = request.get_json()
         print likeData
-        # create_json = json.dumps(likeData, sort_keys=True, indent=4, separators=(',', ': '))
-        json_data = json.loads(likeData)
+        create_json = json.dumps(likeData, sort_keys=True, indent=4, separators=(',', ': '))
+        #json_data = json.loads(likeData)
         # print(json_data)
 
         for i in range(5):
-            artistList.append(json_data["items"][i]["name"])
-            genreList.append(json_data["items"][i]["genres"])
+            artistList.append(create_json["items"][i]["name"])
+            genreList.append(create_json["items"][i]["genres"])
 
         return artistList, genreList
 
